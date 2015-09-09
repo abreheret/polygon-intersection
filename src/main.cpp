1483a3d037eb3b57b1a302e04df23f4076d1b95d
Amaury Bréhéret <abreheret@gmail.com>
2015-09-09 11:12:07 +0200

    factorize and clean source code.

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,14 @@
 #include <vector>
 #include "utils.hpp"
 
-struct PointAngle{
-	CvPoint p;
-	float angle;
-};
-
-int comp_point_with_angle(const void * a, const void * b) {
-	if ( ((PointAngle*)a)->angle <  ((PointAngle*)b)->angle ) return -1;
-	else if ( ((PointAngle*)a)->angle > ((PointAngle*)b)->angle ) return 1;
-	else //if ( ((PointAngle*)a)->angle == ((PointAngle*)b)->angle ) return 0;
-		return 0;
-}
-
 const char * title = "Convex Polygon Intersection";
 class UIPolyIntersection {
 	float _resolution;
 	int _nb_grid;
 	int _width ;
 	int _height;
-	std::vector<CvPoint> _poly[2];
-	std::vector<CvPoint> _inter;
+	Polygon _poly[2];
+	Polygon _inter;
 	std::vector<CvPoint *> _all;
 	IplImage * curr_rgb;
 	CvPoint pCurr;
@@ -35,14 +23,15 @@ class UIPolyIntersection {
 public:
 
 	UIPolyIntersection(int width) { 
-		_nb_grid = 50;
+		_nb_grid = 30;
 		_width = width;
 		_height = _width*9/16;
 		hover_point = selected_point = -1;
 		curr_rgb = cvCreateImage(cvSize(_width,_height),8,3);
 		pCurr = cvPoint(0,0);
 		_resolution = _width /(float)_nb_grid;
-		initRandom();
+		//initRandom();
+		init();
 		cvZero(curr_rgb);
 		cvNamedWindow(title, CV_WINDOW_AUTOSIZE);
 		cvSetMouseCallback(title, &UIPolyIntersection::mouseCallback , this );
@@ -52,18 +41,44 @@ public:
 		cvReleaseImage(&curr_rgb);
 	}
 
+	void init() {
+		_poly[0].clear();
+		_poly[0].add(cvPoint(2* 3*_resolution,2*3*_resolution));
+		_poly[0].add(cvPoint(2*11*_resolution,2*3*_resolution));
+		_poly[0].add(cvPoint(2*11*_resolution,2*6*_resolution));
+		_poly[0].add(cvPoint(2* 1*_resolution,2*6*_resolution));
+		_poly[0].add(cvPoint(2* 1*_resolution,2*4*_resolution));
+			
+		_poly[1].clear();
+		_poly[1].add(cvPoint(2*2*_resolution,2*1*_resolution));
+		_poly[1].add(cvPoint(2*4*_resolution,2*1*_resolution));
+		_poly[1].add(cvPoint(2*4*_resolution,2*5*_resolution));
+		_poly[1].add(cvPoint(2*2*_resolution,2*5*_resolution));
+
+		for (int j = 0 ; j < 2 ; j++ ) {
+			for (int i = 0 ; i < _poly[j].size() ; i++ ) {
+				_all.push_back(&_poly[j][i]);
+			}
+		}
+		updateIntersection();
+	}
+
 	void initRandom() {
 		for (int j = 0 ; j < 2 ; j++ ) {
 			int n = 5;
-			_poly[j].reserve(n);
 			_poly[j].clear();
 			for (int i = 0 ; i < n ; i++ ) {
 				CvPoint p = cvPoint(rand()%_width,rand()%_height);
 				_poly[j].push_back(p);
 				_all.push_back(&_poly[j][i]);
 			}
+			_poly[j].pointsOrdered();
 		}
 		updateIntersection();
+	}
+
+	void updateIntersection() {
+		intersectPolygon(_poly[0],_poly[1],_inter);
 	}
 
 	void mouseEvent( int event, int x, int y, int flags, void* ) {
@@ -106,12 +121,12 @@ public:
 		c[2] = CV_RGB(0,169,255);
 
 		for (int j = 0 ; j < 2 ; j++ ) {
-			for (size_t i = 0 ; i < _poly[j].size() ; i++ ) {
+			for (int i = 0 ; i < _poly[j].size() ; i++ ) {
 				cvDrawLine(curr_rgb,_poly[j][i],_poly[j][(i+1)%_poly[j].size()],c[j],1,CV_AA);
 				sprintf(buff,"%d",i); cvPutText(curr_rgb,buff,_poly[j][i],&font,c[j]);
 			}
-			float area = computeArea(&_poly[j][0],(int)_poly[j].size());
-			CvPoint center = getCenter(&_poly[j][0],(int)_poly[j].size());
+			float area = _poly[j].area();
+			CvPoint center = _poly[j].getCenter();
 			sprintf(buff,"Area = %0.1f",area/(_resolution*_resolution)); cvPutText(curr_rgb,buff,center,&font,c[j]);
 		}
 
@@ -119,16 +134,16 @@ public:
 			cvDrawCircle(curr_rgb,*_all[i],5,CV_RGB(0,255,255),1,CV_AA);
 		}
 
-		for (size_t i = 0 ; i < _inter.size() ; i++ ) {
+		for (int i = 0 ; i < _inter.size() ; i++ ) {
 			cvDrawLine(curr_rgb,_inter[i],_inter[(i+1)%_inter.size()],c[2],2,CV_AA);
 		}
 
-		for (size_t i = 0 ; i < _inter.size() ; i++ ) {
+		for (int i = 0 ; i < _inter.size() ; i++ ) {
 			cvDrawCircle(curr_rgb,_inter[i],1,CV_RGB(250,250,250),2,CV_AA);
 		}
 		if( _inter.size() ) {
-			float area = computeArea(&_inter[0],(int)_inter.size());
-			CvPoint center = getCenter(&_inter[0],(int)_inter.size());
+			float area = _inter.area();
+			CvPoint center = _inter.getCenter();
 			sprintf(buff,"Area intersected = %0.1f",area/(_resolution*_resolution)); cvPutText(curr_rgb,buff,center,&font,c[2]);
 		}
 	}
@@ -149,19 +164,6 @@ public:
 		}
 	}
 
-	CvPoint getCenter(CvPoint * points,int n) {
-		CvPoint center;
-		center.x = 0;
-		center.y = 0;
-		for (int i = 0 ; i < n ; i++ ) {
-			center.x += points[i].x;
-			center.y += points[i].y;
-		}
-		center.x /= n;
-		center.y /= n;
-		return center;
-	}
-
 	float computeArea(const CvPoint * points,int n) {
 		float area0 = 0.;
 		for (int i = 0 ; i < n ; i++ ) {
@@ -172,51 +174,15 @@ public:
 		return 0.5f * abs(area0);
 	}
 
-	void pointsOrdered(CvPoint * points,int n) {
-		CvPoint center = getCenter(points,n);
-		PointAngle pc[400];
-		for (int i = 0 ; i < n ; i++ ) {
-			pc[i].p.x = points[i].x;
-			pc[i].p.y = points[i].y;
-			pc[i].angle = atan2f((float)(points[i].y - center.y),(float)(points[i].x - center.x));
-		}
-		qsort(pc,n,sizeof(PointAngle),comp_point_with_angle);
-		for (int i = 0 ; i < n ; i++ ) {
-			points[i].x = pc[i].p.x;
-			points[i].y = pc[i].p.y;
-		}
-	}
-
-	void updateIntersection() {
-		for (int j = 0 ; j < 2 ; j++ ) {
-			pointsOrdered(&_poly[j][0],(int)_poly[j].size());
-		}
-
-		_inter.clear();
-		for (size_t i = 0 ; i < _poly[0].size() ;i++) {
-			if( pointInPolygon(_poly[0][i],&_poly[1][0],(int)_poly[1].size()) ) 
-				_inter.push_back(_poly[0][i]);
-		}
-
-		for (size_t i = 0 ; i < _poly[1].size() ;i++) { 
-			if( pointInPolygon(_poly[1][i],&_poly[0][0],(int)_poly[0].size()) ) 
-				_inter.push_back(_poly[1][i]);
-		}
-
-		for (size_t i = 0 ; i < _poly[0].size() ;i++) {
-			CvPoint p0,p1,p2,p3;
-			p0 = _poly[0][i];
-			p1 = _poly[0][(i+1)%_poly[0].size()];
-			for (size_t j = 0 ; j < _poly[1].size() ;j++) {
-				p2 = _poly[1][j];
-				p3 = _poly[1][(j+1)%_poly[1].size()];
-				CvPoint inter;
-				if(segementIntersection(p0,p1,p2,p3,&inter)) {
-					_inter.push_back(inter);
-				}
+	void savePolygons() {
+		FILE * file = fopen("log.txt","w");
+		for (int i = 0 ; i < 2 ; i++) {
+			fprintf(file,"%d\n",_poly[i].size());
+			for (int j = 0 ; j < _poly[i].size() ; j++) {
+				fprintf(file,"%d %d\n",_poly[i][j].x,_poly[i][j].y);
 			}
 		}
-		if(_inter.size())pointsOrdered(&_inter[0],(int)_inter.size());
+		fclose(file);
 	}
 
 	void run() {
@@ -229,6 +195,7 @@ public:
 			cvShowImage(title,curr_rgb);
 			switch( key = cvWaitKey(30)) {
 			case 'r' : initRandom();break;
+			case 's' : savePolygons();break;
 			case 27  : return ;
 			}
 		}
@@ -237,7 +204,7 @@ public:
 
 int main(int argc,char **argv) {
 	try {
-		UIPolyIntersection ui(1280);
+		UIPolyIntersection ui(1200);
 		ui.run();
 	} catch (std::exception &e) {
 		printf("Err : %s\n",e.what());
--- src/utils.cpp
+++ src/utils.cpp
@@ -65,7 +65,7 @@ bool segementIntersection(CvPoint2D32f p0,CvPoint2D32f p1,CvPoint2D32f p2,CvPoin
 	return true;
 }
 
-bool pointInPolygon(CvPoint2D32f p, CvPoint2D32f * points,int n) {
+bool pointInPolygon(CvPoint2D32f p,const CvPoint2D32f * points,int n) {
 	int i, j ;
 	bool c = false;
 	for(i = 0, j = n - 1; i < n; j = i++) {
@@ -77,7 +77,7 @@ bool pointInPolygon(CvPoint2D32f p, CvPoint2D32f * points,int n) {
 	return c;
 }
 
-bool pointInPolygon(CvPoint p, CvPoint * points,int n) {
+bool pointInPolygon(CvPoint p,const CvPoint * points,int n) {
 	int i, j ;
 	bool c = false;
 	for(i = 0, j = n - 1; i < n; j = i++) {
@@ -87,4 +87,100 @@ bool pointInPolygon(CvPoint p, CvPoint * points,int n) {
 			c = !c;
 	}
 	return c;
+}
+
+
+float computeArea(const CvPoint * pt,int n ) {
+	float area0 = 0.f;
+	for (int i = 0 ; i < n ; i++ ) {
+		int j = (i+1)%n;
+		area0 += pt[i].x * pt[j].y;
+		area0 -= pt[i].y * pt[j].x;
+	}
+	return 0.5f * fabs(area0);
+}
+
+struct PointAngle{
+	CvPoint p;
+	float angle;
+};
+
+CvPoint Polygon::getCenter() const {
+	CvPoint center;
+	center.x = 0;
+	center.y = 0;
+	for (int i = 0 ; i < n ; i++ ) {
+		center.x += pt[i].x;
+		center.y += pt[i].y;
+	}
+	center.x /= n;
+	center.y /= n;
+	return center;
+}
+
+static int comp_point_with_angle(const void * a, const void * b) {
+	if ( ((PointAngle*)a)->angle <  ((PointAngle*)b)->angle ) return -1;
+	else if ( ((PointAngle*)a)->angle > ((PointAngle*)b)->angle ) return 1;
+	else //if ( ((PointAngle*)a)->angle == ((PointAngle*)b)->angle ) return 0;
+		return 0;
+}
+
+float Polygon::area() const {
+	return computeArea(pt,n);
+}
+
+void Polygon::pointsOrdered() {
+	if( n <= 0) return;
+	CvPoint center = getCenter();
+	PointAngle pc[MAX_POINT_POLYGON];
+	for (int i = 0 ; i < n ; i++ ) {
+		pc[i].p.x = pt[i].x;
+		pc[i].p.y = pt[i].y;
+		pc[i].angle = atan2f((float)(pt[i].y - center.y),(float)(pt[i].x - center.x));
+	}
+	qsort(pc,n,sizeof(PointAngle),comp_point_with_angle);
+	for (int i = 0 ; i < n ; i++ ) {
+		pt[i].x = pc[i].p.x;
+		pt[i].y = pc[i].p.y;
+	}
+}
+
+bool Polygon::pointIsInPolygon(CvPoint p) const {
+	return pointInPolygon(p,pt,n);
+}
+
+//
+
+void intersectPolygon( const CvPoint * poly0, int n0,const CvPoint * poly1,int n1, Polygon & inter )  {
+	inter.clear();
+	for (int i = 0 ; i < n0 ;i++) {
+		if( pointInPolygon(poly0[i],poly1,n1) ) {
+			inter.add(poly0[i]);
+		}
+	}
+
+	for (int i = 0 ; i < n1 ;i++) { 
+		if( pointInPolygon(poly1[i],poly0,n0) ) 
+			inter.add(poly1[i]);
+	}
+
+	for (int i = 0 ; i < n0 ;i++) {
+		CvPoint p0,p1,p2,p3;
+		p0 = poly0[i];
+		p1 = poly0[(i+1)%n0];
+		for (int j = 0 ; j < n1 ;j++) {
+			p2 = poly1[j];
+			p3 = poly1[(j+1)%n1];
+			CvPoint pinter;
+			if(segementIntersection(p0,p1,p2,p3,&pinter)) {
+				inter.add(pinter);
+			}
+		}
+	}
+	inter.pointsOrdered();
+}
+
+
+void intersectPolygon( const Polygon & poly0, const Polygon & poly1, Polygon & inter ) {
+	intersectPolygon(&poly0[0],poly0.size(),&poly1[0],poly1.size(),inter);
 }
